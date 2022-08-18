b4c0fa4dcb4575d7c06e1ae029542ccdaed56e53
Greshch <greshchook@gmail.com>
2022-08-18 07:15:33 +0300

    Horse was implemented

--- Horse.cpp
+++ Horse.cpp
@@ -1,4 +1,5 @@
 #include "Horse.h"
+#include <cmath>
 
 Horse::Horse() : Figure()
 {
@@ -12,5 +13,30 @@ Horse::Horse(int xPos, int yPos) : Figure(xPos, yPos)
 
 bool Horse::CanMoveToPosition(int xPos, int yPos)
 {
-    return false;
+    if (xPos < 0 || xPos >= FieldCols) // check cols
+    {
+        return false;
+    }
+
+    if (yPos < 0 || yPos >= FieldRows) // check rows
+    {
+        return false;
+    }
+
+    int dx = abs(m_xPos - xPos);
+    int dy = abs(m_yPos - yPos);
+    int const litleStep = 1;
+    int const bigStep = 2;
+
+    if (dx != litleStep && dx != bigStep)
+    {
+        return false;
+    }
+
+    if (dy != litleStep && dy != bigStep)
+    {
+        return false;
+    }
+
+    return true;
 }
